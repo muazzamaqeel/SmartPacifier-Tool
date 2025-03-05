29ba4116aa3944d4e460e02117daa93bf72a4808
Muazzam <muazzamaqeel@outlook.com>
2025-03-05 23:33:38 +0100

    Updates

--- src/service_backend/src/ipc/nng/NNGHandler.cpp
+++ src/service_backend/src/ipc/nng/NNGHandler.cpp
@@ -1,19 +1,19 @@
-//
-// Created by muazz on 02/03/2025.
-//
+    //
+    // Created by muazz on 02/03/2025.
+    //
 
-#include "NNGHandler.h"
+    #include "NNGHandler.h"
 
-#include <iostream>
+    #include <iostream>
 
-NNGHandler::NNGHandler() {
-    // Initialize NNG resources if needed.
-}
+    NNGHandler::NNGHandler() {
+        // Initialize NNG resources if needed.
+    }
 
-NNGHandler::~NNGHandler() {
-    // Clean up NNG resources if needed.
-}
+    NNGHandler::~NNGHandler() {
+        // Clean up NNG resources if needed.
+    }
 
-void NNGHandler::send_message(const std::string& message) {
-    std::cout << "NNGHandler: sending message: " << message << std::endl;
-}
+    void NNGHandler::send_message(const std::string& message) {
+        std::cout << "NNGHandler: sending message: " << message << std::endl;
+    }
--- src/service_backend/src/ipc/nng/NNGHandler.h
+++ src/service_backend/src/ipc/nng/NNGHandler.h
@@ -1,14 +1,22 @@
-#ifndef SERVICE_BACKEND_NNGHANDLER_H
-#define SERVICE_BACKEND_NNGHANDLER_H
+#ifndef NNG_HANDLER_H
+#define NNG_HANDLER_H
 
 #include <string>
+
+// Make sure CMake points to the directory that has these headers:
+#include <nng/nng.h>
+#include <nng/protocol/pubsub0/pub.h>
 
 class NNGHandler {
 public:
     NNGHandler();
     ~NNGHandler();
 
+    // Publish a message via NNG pub socket
     void send_message(const std::string& message);
+
+private:
+    nng_socket pub_socket_;
 };
 
-#endif //SERVICE_BACKEND_NNGHANDLER_H
+#endif // NNG_HANDLER_H
