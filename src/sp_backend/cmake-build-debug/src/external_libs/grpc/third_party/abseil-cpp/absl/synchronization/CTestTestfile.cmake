bde32b32de8283c156f5914a757c003330633cc2
SimonE <simon.engel@de.zuken.com>
2025-03-26 13:35:00 +0100

    Updated gitignore to ignore build files & removed comitted build files

--- src/sp_backend/cmake-build-debug/src/external_libs/grpc/third_party/abseil-cpp/absl/synchronization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Programming/SmartPacifier-Tool/src/sp_backend/src/external_libs/grpc/third_party/abseil-cpp/absl/synchronization
-# Build directory: C:/Programming/SmartPacifier-Tool/src/sp_backend/cmake-build-debug/src/external_libs/grpc/third_party/abseil-cpp/absl/synchronization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
