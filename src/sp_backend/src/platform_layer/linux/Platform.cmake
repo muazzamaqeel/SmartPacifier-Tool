b90f52fa94b08ae864b57b61a2b8e044940a7175
Muazzam Bin Aqeel <muazzamaqeel14@gmail.com>
2025-04-06 21:55:44 +0200

    CMake Linux

--- src/sp_backend/src/platform_layer/linux/Platform.cmake
+++ src/sp_backend/src/platform_layer/linux/Platform.cmake
@@ -1,2 +1,10 @@
 # Linux Platform configuration
 message(STATUS "Configuring for Linux platform")
+# Adding extra compile warnings for Linux.
+add_compile_options(-Wall -Wextra -Wpedantic)
+
+# Potential Location of the Installed Libraries - (Will be updated on GitHub, as we are still required to Update the Building Process of the Libraries on the Linux Machine)
+
+# set(CMAKE_PREFIX_PATH "/usr/local/grpc" CACHE PATH "" FORCE)
+# set(gRPC_DIR "/usr/local/grpc/lib/cmake/grpc" CACHE PATH "" FORCE)
+# set(Protobuf_DIR "/usr/local/grpc/lib/cmake/protobuf" CACHE PATH "" FORCE)
