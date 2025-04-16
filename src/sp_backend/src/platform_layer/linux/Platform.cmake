826350e47bd6283cc7f4336737462d4175d5a4fa
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2025-04-16 21:50:35 +0200

    Update src/sp_backend/src/platform_layer/linux/Platform.cmake
    Co-authored-by: Sebastian Moosbauer <sebastian.moosbauer@thu.de>

--- src/sp_backend/src/platform_layer/linux/Platform.cmake
+++ src/sp_backend/src/platform_layer/linux/Platform.cmake
@@ -3,7 +3,7 @@ message(STATUS "Configuring for Linux platform")
 # Adding extra compile warnings for Linux.
 add_compile_options(-Wall -Wextra -Wpedantic)
 
-# Potential Location of the Installed Libraries - (Will be updated on GitHub, as we are still required to Update the Building Process of the Libraries on the Linux Machine)
+# TODO (basti, muazzam): check build process on linux and modify below paths accordingly.
 
 # set(CMAKE_PREFIX_PATH "/usr/local/grpc" CACHE PATH "" FORCE)
 # set(gRPC_DIR "/usr/local/grpc/lib/cmake/grpc" CACHE PATH "" FORCE)
