870cdddbf4c07ea938a48762a7469b7cd2ebf503
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2025-04-16 21:32:16 +0200

    Update src/sp_backend/src/communication_layer/broker/Logger.cpp
    Co-authored-by: Sebastian Moosbauer <sebastian.moosbauer@thu.de>

--- src/sp_backend/src/communication_layer/broker/Logger.cpp
+++ src/sp_backend/src/communication_layer/broker/Logger.cpp
@@ -22,6 +22,6 @@ Logger::~Logger() {
 void Logger::log(const std::string& message) {
     std::lock_guard<std::mutex> lock(logMutex);
     auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    logFile << std::ctime(&now) << " - " << message << std::endl;
+    logFile << std::ctime(&now) << " - " << message << "\n";
     std::cout << message << std::endl;
 }
