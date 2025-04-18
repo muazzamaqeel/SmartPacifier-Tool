d84da01732552bbbe39d696b4ee2659663adf894
Muazzam Bin Aqeel <muazzamaqeel14@gmail.com>
2025-04-18 22:35:29 +0100

    Rule of 5

--- src/sp_backend/src/platform_layer/PlatformManager.h
+++ src/sp_backend/src/platform_layer/PlatformManager.h
@@ -2,7 +2,13 @@
 
 class PlatformManager {
 public:
+    PlatformManager() = default;
     virtual ~PlatformManager() = default;
+
+    PlatformManager(const PlatformManager&) = delete;
+    PlatformManager& operator=(const PlatformManager&) = delete;
+    PlatformManager(PlatformManager&&) noexcept = delete;
+    PlatformManager& operator=(PlatformManager&&) noexcept = delete;
+
     virtual void runBackend() = 0;
 };
-
