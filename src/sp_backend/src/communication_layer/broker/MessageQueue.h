1356376442c9775451704e075094aab82d87e50f
Muazzam Bin Aqeel <muazzamaqeel14@gmail.com>
2025-04-23 16:07:12 +0100

    Renaming Variables

--- src/sp_backend/src/communication_layer/broker/MessageQueue.h
+++ src/sp_backend/src/communication_layer/broker/MessageQueue.h
@@ -20,38 +20,38 @@ namespace broker {
         // Push an item; wake any pop() waiters and fire batch callback if threshold met.
         void push(T item) {
             {
-                std::lock_guard<std::mutex> lk(mutex_);
-                queue_.push(std::move(item));
+                std::lock_guard<std::mutex> lk(m_mutex);
+                m_queue.push(std::move(item));
             }
-            cv_.notify_one();
+            m_cv.notify_one();
 
-            if (batchCallback_ && queue_.size() >= batchThreshold_) {
+            if (m_batchCallback && m_queue.size() >= m_batchThreshold) {
                 std::vector<T> batch;
                 {
-                    std::lock_guard<std::mutex> lk(mutex_);
-                    for (size_t i = 0; i < batchThreshold_; ++i) {
-                        batch.push_back(std::move(queue_.front()));
-                        queue_.pop();
+                    std::lock_guard<std::mutex> lk(m_mutex);
+                    for (size_t i = 0; i < m_batchThreshold; ++i) {
+                        batch.push_back(std::move(m_queue.front()));
+                        m_queue.pop();
                     }
                 }
-                batchCallback_(batch);
+                m_batchCallback(batch);
             }
         }
 
         // Block until at least one item is available, then pop and return it.
         T pop() {
-            std::unique_lock<std::mutex> lk(mutex_);
-            cv_.wait(lk, [&]{ return !queue_.empty(); });
-            T item = std::move(queue_.front());
-            queue_.pop();
+            std::unique_lock<std::mutex> lk(m_mutex);
+            m_cv.wait(lk, [&]{ return !m_queue.empty(); });
+            T item = std::move(m_queue.front());
+            m_queue.pop();
             return item;
         }
 
         // Register a callback to fire once `threshold` items accumulate.
         void setBatchCallback(BatchCallback cb, size_t threshold) {
-            std::lock_guard<std::mutex> lk(mutex_);
-            batchCallback_  = std::move(cb);
-            batchThreshold_ = threshold;
+            std::lock_guard<std::mutex> lk(m_mutex);
+            m_batchCallback  = std::move(cb);
+            m_batchThreshold = threshold;
         }
 
     private:
