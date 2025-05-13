79505d1acb926922ee09396baf7b858cb8ca09ae
Muazzam Bin Aqeel <muazzamaqeel14@gmail.com>
2025-05-13 16:15:53 +0200

    Remove LOC

--- src/sp_backend/src/communication_layer/CommunicationLayer.cpp
+++ src/sp_backend/src/communication_layer/CommunicationLayer.cpp
@@ -29,14 +29,10 @@ void CommunicationLayer::startCommunicationServices() {
         return;
     }
     Logger::getInstance().log("Mosquitto running.");
-
     running_ = true;
-
     // Create and own the gRPC service so it outlives this function
     grpcService_ = std::make_unique<GrpcService>(broker::globalQueue);
-
     // Batch‐callback: Forwarding each batch into the service
-
     //Value 1 means no batching each message is delivered immediately
     //Value 5 means for example, the call back would wait until 5 messages are queued then send those 5 values at once.
     constexpr size_t batchSize = 1;
@@ -46,15 +42,12 @@ void CommunicationLayer::startCommunicationServices() {
         },
         batchSize
     );
-
     // Push incoming MQTT payloads into the MessageQueue
     dataRetrieval_->setMessageCallback([](const std::string &msg) {
         broker::globalQueue.push(msg);
     });
-
     // Start MQTT client thread
     mqttThread_ = std::thread(&CommunicationLayer::runMqttClient, this);
-
     // Start gRPC server thread
     grpcThread_ = std::thread(&CommunicationLayer::runGrpcServer, this);
 }
